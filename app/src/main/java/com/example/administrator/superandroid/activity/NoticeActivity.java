3664bfd1030774dc6bf22c87be0911f90012442f
zhanglong <zhanglong@eshippinggateway.com>
2017-04-24 17:50:24 +0800

    jiejing

--- app/src/main/java/com/example/administrator/superandroid/activity/NoticeActivity.java
+++ app/src/main/java/com/example/administrator/superandroid/activity/NoticeActivity.java
@@ -2,14 +2,21 @@ package com.example.administrator.superandroid.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 import com.example.administrator.superandroid.R;
+import com.example.administrator.superandroid.util.ConfigUtil;
 
 public class NoticeActivity extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_notice);
+        setContentView(R.layout.activity_library);
+        webView = (WebView) findViewById(R.id.library);
+
+        webView.loadUrl(ConfigUtil.getValueByKey(getApplicationContext(),"news.url"));
+
     }
 }
